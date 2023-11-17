e402901daaaa6457ebf2e2671f630c3f0c188c7f
Ayeniko Emmanuel <ayenikoemmanuel06@gmail.com>
2023-11-17 00:57:28 +0000

    updated dictionary

--- components/ForHomesPage/AccordionForHomes.tsx
+++ components/ForHomesPage/AccordionForHomes.tsx
@@ -1,30 +1,36 @@
+"use client";
+
 import Accordion from "@mui/material/Accordion";
 import AccordionSummary from "@mui/material/AccordionSummary";
 import AccordionDetails from "@mui/material/AccordionDetails";
 import Typography from "@mui/material/Typography";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { dictionary } from "@/content";
+import { useAppContext } from "@/context/AppContext";
 
 export default function AccordionForHomes() {
+  const { state } = useAppContext();
+
   const accordionDetails = [
     {
-      title: "Reduced Energy Bills",
-      desc: "A residential solar power plant allows homeowners to generate their electricity, leading to a significant reduction in monthly energy bills. By harnessing the power of the sun, households can offset or even eliminate their dependence on traditional utility providers.",
+      title: dictionary[state]?.reducedEnergyTitle,
+      desc: dictionary[state]?.reducedEnergyDesc,
     },
     {
-      title: "Long-Term Investment",
-      desc: "While the initial investment in a solar power system might seem significant, it's essential to view it as a long-term investment. Over time, the financial savings accrued from lower energy bills can surpass the initial setup costs, potentially resulting in a profitable venture.",
+      title: dictionary[state]?.longTermInvestmentTitle,
+      desc: dictionary[state]?.longTermInvestmentDesc,
     },
     {
-      title: "Increased Property Value",
-      desc: "Homes equipped with solar power systems tend to have higher property values. The presence of a sustainable and energy-efficient feature is attractive to potential buyers and reflects positively on the overall value of the property. This can be a key selling point in the real estate market.",
+      title: dictionary[state]?.increasedPropertyValueTitle,
+      desc: dictionary[state]?.increasedPropertyValueDesc,
     },
     {
-      title: "Energy Independence",
-      desc: "Households with solar power systems gain a degree of energy independence. This is especially valuable during power outages or disruptions to the traditional grid. With a reliable solar energy source, homeowners can ensure a continuous power supply for essential appliances.",
+      title: dictionary[state]?.energyIndependenceTitle,
+      desc: dictionary[state]?.energyIndependenceDesc,
     },
     {
-      title: "Low Maintenance Costs",
-      desc: "Solar power systems generally have low maintenance costs. Regular cleaning and occasional checks are usually sufficient to keep the system running efficiently. This contributes to the overall cost-effectiveness of residential solar power installations.",
+      title: dictionary[state]?.lowMaintenanceCostsTitle,
+      desc: dictionary[state]?.lowMaintenanceCostsDesc,
     },
   ];
   return (
--- components/ForHomesPage/ForHomes.tsx
+++ components/ForHomesPage/ForHomes.tsx
@@ -18,25 +18,21 @@ const ForHomes = () => {
           height={2000}
           alt="for-homes"
           className="w-full md:w-[80%] mx-auto lg:w-[60rem] mt-[1rem]"
+          loading="lazy"
         />
         <h1 className="lg:text-[2.2rem] text-[1.8rem] font-medium lg:font-normal leading-[2.3rem] mt-[2.5rem] lg:mt-[4rem] lg:leading-[3.5rem] md:textcenter lg:w[90%]">
-          A solar power plant on a private property is a profitable investment
+          {dictionary[state]?.homesHeading}
         </h1>
         <p className="my-[1.5rem] opacity-80 lg:text-[1.1rem]">
-          With a solar power plant, you can start producing electricity right
-          away, save money on energy bills, and even profit from selling excess
-          power. The clean energy source of the future is solar electricity.
+          {dictionary[state]?.homesP}
         </p>
         <div className="flex flex-col lg:flex-row gap-[2rem] lg:itemscenter mt-[3rem] lg:mt-[7rem] lg:mb-[2rem]">
           <div className="lg:w-[50%]">
             <h1 className="text-[1.5rem] lg:text-[2rem] font-light underline">
-              The solar power plant owned by the household
+              {dictionary[state]?.homesHeading2}
             </h1>
             <p className="opacity-80 mt-[1rem] lg:text-[1.1rem]">
-              Establishing a solar power system at the household level is a
-              smart investment with a range of tangible benefits. Let's explore
-              the advantages of having a residential solar power plant in more
-              detail:
+              {dictionary[state]?.homesP2}
             </p>
           </div>
           <div className="lg:w-[50%]">
