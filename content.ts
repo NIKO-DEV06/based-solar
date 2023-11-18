d4b18d2806cae1de8716798f492e060115f7b269
Antonio Iliev <skylandbg32@gmail.com>
2023-11-18 15:44:26 +0200

    changed-bg

--- content.ts
+++ content.ts
@@ -95,10 +95,98 @@ export const dictionary: any = {
       "Solar power systems generally have low maintenance costs. Regular cleaning and occasional checks are usually sufficient to keep the system running efficiently. This contributes to the overall cost-effectiveness of residential solar power installations.",
   },
   bg: {
-    homeNav: "за домове",
-    businessNav: "Бизнес",
-    projectsNav: "проекти",
-    contactNav: "контакт",
-    homeHeader: "y дома",
+    homeNav: "За домове",
+    businessNav: "За бизнеси",
+    projectsNav: "Проекти",
+    contactNav: "Контакти",
+    homeHeader: "Home",
+    heroExperience: "10+ YEARS OF EXPERIENCE",
+    basedSolarHero: "Based Solar:",
+    heroHeader: "Your Gateway to Clean, Cost-Efficient Energy Solutions",
+    heroP:
+      "Curious about the potential of solar energy for your home or business? Get a free quote from Based Solar today and discover how you can save money and the planet.",
+    getAQuote: "Get a Quote",
+    aboutUs: "About Us",
+    aboutUsParagraph:
+      "Welcome to Based Solar: Your Gateway to Clean, Cost-Efficient Energy Solutions.Our green power services at Based Solar offer clean, sustainable energy solutions for businesses and homeowners. We provide state-of-the-art photovoltaic systems, energy efficiency, and a greener future, all while reducing costs and environmental impact. Empower your future with our premier photovoltaic systems.",
+    letsGoSolar: "Let's go solar today!",
+    reducedBill: "Reduced bills by up to 70%",
+    reducedBillP:
+      "Photovoltaic systems use sunlight to produce electricity. Appliances in homes and other users can directly use the energy that is produced. It can also be retrieved and stored into the national network.",
+    energy: "Energy Self-Sufficiency",
+    energyP:
+      "Regarding the energy bill, photovoltaic panels provide stability and predictability in the current climate where bills are expensive and vary annually.",
+    whatDoWeOffer: " What do we offer ",
+    toYou: " to you",
+    forHouses: "For Houses",
+    forHousesP:
+      "Elevate your home with our tailored solutions, providing unmatched quality and reliability.",
+    forBusiness: "For Businesses",
+    forBusinessP:
+      "Empower your business with our specialized services, tailored to meet the unique needs of commercial spaces.",
+    forAssociation: "For Housing Associations",
+    forAssociationP:
+      "Our solutions for housing associations focus on sustainable, community-centric initiatives to enhance the well-being of residents.",
+    gurantee: "30 Years Guarantee",
+    guranteeP:
+      "With a solid 30-year guarantee, our products and services are built to last, providing you with long-term peace of mind and satisfaction.",
+    demand: "Demand Response",
+    demandP:
+      "Our demand response solutions empower you to manage and optimize energy usage, ensuring efficiency and cost-effectiveness in real-time.",
+    capacity: "Capacity Reserve",
+    capacityP:
+      "We offer reliable solutions to meet peak demand, ensuring a steady and resilient power supply when you need it the most.",
+    projects: "Projects",
+    projectsP: "Take a look at some of our completed projects.",
+    viewProjects: "View all projects",
+    getInTouch: "Empower your future with our premier photovoltaic systems",
+    getInTouchP:
+      "Curious about the potential of solar energy for your home or business? Get a free quote from Based Solar today and discover how you can save money and the planet.",
+    contactUs: "Contact Us (Bulgaria, Sofia)",
+    phone: "Phone",
+    rights: "2023 Based Solar. All Rights Reserved",
+    privacyPolicy: "Privacy Policy",
+    poweredBy: "Powered by",
+    welcome: "Welcome",
+    menu: "Menu",
+    getInTouchText: "Get in touch",
+    keepScrolling: "Keep scrolling to view projects",
+    letUsKnow: "Let us know more about",
+    your: "your",
+    solarPlan: "solar plan",
+    yourName: "Your name",
+    yourEmail: "Your email",
+    yourProject: "Tell us about your project",
+    submit: "Submit",
+    sending: "Sending...",
+    openSlider: "Click image to open slider",
+    homesHeading:
+      "A solar power plant on a private property is a profitable investment",
+    homesP:
+      "With a solar power plant, you can start producing electricity right away, save money on energy bills, and even profit from selling excess power. The clean energy source of the future is solar electricity.",
+    homesHeading2: "The solar power plant owned by the household",
+    homesP2:
+      "Establishing a solar power system at the household level is a smart investment with a range of tangible benefits. Let's explore the advantages of having a residential solar power plant in more detail:",
+    businessHeading: "Lower energy expenses for your business with solar power",
+    businessP:
+      "The yield of the solar power plant is evident in the electricity bill savings. The electricity generated by solar power plants is environmentally friendly and represents the energy source of the future.",
+    businessHeading2: "An investment that pays off is the solar power plant.",
+    businessP2:
+      "Establishing a solar power system at the household level is a smart investment with a range of tangible benefits. Let's explore the advantages of having a residential solar power plant in more detail:",
+    reducedEnergyTitle: "Reduced Energy Bills",
+    reducedEnergyDesc:
+      "A residential solar power plant allows homeowners to generate their electricity, leading to a significant reduction in monthly energy bills. By harnessing the power of the sun, households can offset or even eliminate their dependence on traditional utility providers.",
+    longTermInvestmentTitle: "Long-Term Investment",
+    longTermInvestmentDesc:
+      "While the initial investment in a solar power system might seem significant, it's essential to view it as a long-term investment. Over time, the financial savings accrued from lower energy bills can surpass the initial setup costs, potentially resulting in a profitable venture.",
+    increasedPropertyValueTitle: "Increased Property Value",
+    increasedPropertyValueDesc:
+      "Homes equipped with solar power systems tend to have higher property values. The presence of a sustainable and energy-efficient feature is attractive to potential buyers and reflects positively on the overall value of the property. This can be a key selling point in the real estate market.",
+    energyIndependenceTitle: "Energy Independence",
+    energyIndependenceDesc:
+      "Households with solar power systems gain a degree of energy independence. This is especially valuable during power outages or disruptions to the traditional grid. With a reliable solar energy source, homeowners can ensure a continuous power supply for essential appliances.",
+    lowMaintenanceCostsTitle: "Low Maintenance Costs",
+    lowMaintenanceCostsDesc:
+      "Solar power systems generally have low maintenance costs. Regular cleaning and occasional checks are usually sufficient to keep the system running efficiently. This contributes to the overall cost-effectiveness of residential solar power installations.",
   },
 };
