7468ecfad35ebed773c17418b360961775d7eb29
Ayeniko Emmanuel <ayenikoemmanuel06@gmail.com>
2023-11-18 18:13:04 +0000

    updated config with envs

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  env: {
+    NEXT_SERVICE_ID: process.env.NEXT_SERVICE_ID,
+    NEXT_TEMPLATE_ID: process.env.NEXT_TEMPLATE_ID,
+    NEXT_KEY_ID: process.env.NEXT_KEY_ID,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
